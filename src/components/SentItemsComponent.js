a6e0475124224f5eda9823540f59a3ac124e28f5
Sujal <ss6sujal@gmail.com>
2020-10-17 21:09:54 +0530

    formatting changes

--- src/components/SentItemsComponent.js
+++ src/components/SentItemsComponent.js
@@ -65,16 +65,16 @@ class SentItemsComponent extends Component {
         this.forceUpdate();
     }
     //find out if any kind of calculation is required
-    calculationRequired = (rows) => {
+    calculationRequired = (rows,priceMap) => {
         var calculationRequired = false;
         rows.forEach((row) => {
             //user has selected the name & the api has fetched the value but the state variable mPrice is not updated
-            if (row.name !== "" && this.props.priceMap[row.name] !== row.mPrice) {
+            if (row.name !== "" && priceMap[row.name] !== row.mPrice) {
                 calculationRequired = true;
                 return;
             }
             //if everything is updated but the stateVariable tPrice is not updated
-            if (row.name !== "" && this.props.priceMap[row.name] === row.mPrice
+            if (row.name !== "" && priceMap[row.name] === row.mPrice
                 && row.qty !== 0 && row.tPrice !== (parseInt(row.mPrice) * parseInt(row.qty))) {
                 calculationRequired = true;
                 return;
@@ -85,7 +85,7 @@ class SentItemsComponent extends Component {
 
     //update the market price in the array after API has fetched the price in the priceMap
     componentDidUpdate(prevProps, prevState) {
-        if (this.calculationRequired(prevState.rows)) {
+        if (this.calculationRequired(prevState.rows,prevProps.priceMap)) {
             this.setState({
                 totalPrice: getTotalPrice(prevState.rows, prevProps.priceMap),
                 rows: getUpdatedRowData(prevState.rows, prevProps.priceMap)
@@ -97,7 +97,7 @@ class SentItemsComponent extends Component {
         return (
             <Container>
                 <Row>
-                    <Col><h4>Items sent</h4></Col>
+                    <Col><h4>Sent</h4></Col>
                 </Row>
                 <Row>
                     <Table id="sentListTable">
@@ -117,7 +117,7 @@ class SentItemsComponent extends Component {
                                         <td>
                                             <Typeahead id={"name_" + row.id} maxResults={5} disabled={this.props.itemNameList === null} onChange={(selected) => { this.updateTypeAheadSelectedName(selected, row.id) }} options={this.props.itemNameList === null ? [] : this.props.itemNameList} />
                                         </td>
-                                        <td><Input type="number" disabled={this.props.itemNameList === null} name={"qty_" + row.id} value={row.qty} onChange={this.updateQty} /></td>
+                                        <td><Input type="number" disabled={this.props.itemNameList === null} name={"qty_" + row.id} value={row.qty} onChange={this.updateQty} min={0}/></td>
                                         <td><Input type="number" name={"mPrice_" + row.id} value={row.mPrice} disabled={true} /></td>
                                         <td><Input type="number" name={"tPrice_" + row.id} value={row.tPrice} disabled={true} /></td>
                                         <td>
--- src/utils/priceMapGenerator.js
+++ src/utils/priceMapGenerator.js
@@ -1,14 +1,14 @@
-export function populateDefaultPriceMap(){
-    if(localStorage.getItem("MARKET_ITEMS_SIMPLE")){
-        var itemNames=JSON.parse(localStorage.getItem("MARKET_ITEMS_SIMPLE"));
-        var defaultPriceMap={};
-        itemNames.forEach((itemName)=>{
-            defaultPriceMap[itemName]=0;
+export function populateDefaultPriceMap() {
+    if (localStorage.getItem("MARKET_ITEMS_SIMPLE")) {
+        var itemNames = JSON.parse(localStorage.getItem("MARKET_ITEMS_SIMPLE"));
+        var defaultPriceMap = {};
+        itemNames.forEach((itemName) => {
+            defaultPriceMap[itemName] = 0;
         });
         console.log("price map initialised successfully!");
         return defaultPriceMap;
     }
-    else{
+    else {
         console.error("price map failed to initialise as no items metadata was found!");
     }
 }
